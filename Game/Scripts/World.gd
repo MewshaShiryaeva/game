984a6de9f106c899f53a78b9da1f6df8a4acc38d
Loopeq <arsen.00z11@gmail.com>
2022-11-06 17:30:40 +0300

    fix3  баляя

--- Game/Scripts/World.gd
+++ Game/Scripts/World.gd
@@ -1,9 +1,13 @@
 extends Node
 
 var Mob = preload("res://Scenes/EntitiesScene/spidor.tscn")
-
+var Player = preload("res://Scenes/EntitiesScene/Player.tscn")
 
 func _ready():
+	var player = Player.instance()
+	player.position.x = 300
+	player.position.y = 300
+	add_child(player)
 	for i in range(0 ,10):
 		var mob = Mob.instance()
 		mob.position.x = 500
