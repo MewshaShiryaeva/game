839e172c08a302377fdc66d7fde188d3c0d7aea9
Loopeq <arsen.00z11@gmail.com>
2022-11-07 22:46:01 +0300

    фор маша

--- Game/Scripts/World.gd
+++ Game/Scripts/World.gd
@@ -1,17 +1,26 @@
 extends Node
 
-var Mob = preload("res://Scenes/EntitiesScene/spidor.tscn")
-var Player = preload("res://Scenes/EntitiesScene/Player.tscn")
+
+onready var Player = preload("res://Scenes/EntitiesScene/Player.tscn")
+onready var Enemy = preload("res://Scenes/EntitiesScene/Enemy.tscn")
 
 func _ready():
 	var player = Player.instance()
 	player.position.x = 256
 	player.position.y = 960
 	add_child(player)
-	for i in range(0 ,10):
-		var mob = Mob.instance()
-		var spawn_position = $Path2D/PathFollow2D
-		spawn_position.offset = randi()
-		mob.position = spawn_position.position
-		add_child(mob)
+	for i in range(0 , 4):
+		get_mob()	
 	
+
+func _process(delta):
+	pass
+
+func get_mob():
+	var mob = Enemy.instance()
+	var spawn_position = $Path2D1/PathFollow2D
+	spawn_position.offset = randi()
+	mob.x_clamp = spawn_position.position.x
+	mob.y_clamp = spawn_position.position.y
+	mob.position = spawn_position.position
+	add_child(mob)
--- Game/Scripts/spidor.gd
+++ Game/Scripts/spidor.gd
@@ -1,9 +1,15 @@
 extends KinematicBody2D
 
+
+
 var staying = true
 var speed = 45
 var velocity = Vector2()
 var destination = Vector2()
+var prev_pos = Vector2()
+export var x_clamp = 0
+export var y_clamp = 0
+
 
 func _ready():
 	pass
@@ -22,10 +28,12 @@ func select_animation():
 
 func _process(delta):
 	if velocity:
+		prev_pos = position
 		select_animation()
 		move_and_slide(velocity)
 	generate_destination()
-			
+	search_player()
+	
 func get_destination(dest):
 	destination = dest
 	velocity = (destination - position).normalized() * speed
@@ -34,13 +42,22 @@ func get_destination(dest):
 func generate_destination():	
 	if staying:
 		randomize()
-		var x = rand_range(position.x - 150, position.x + 150)
-		var y = rand_range(position.y - 150, position.y + 150)
+	
+		var x = rand_range(x_clamp - 90, x_clamp + 90)
+		var y = rand_range(y_clamp - 90, y_clamp + 90)
 		
 		get_destination(Vector2(x, y))
 	elif velocity != Vector2():
 		if position.distance_to(destination) <= speed:
 			cancel_moving()
+		elif prev_pos.distance_to(position) <= 0.6:
+			cancel_moving()
+
+
+func search_player():
+	var pl_pos = Vector2(global.player_pos.x, global.player_pos.y)
+	if pl_pos.distance_to(position) <= 200:
+		get_destination(pl_pos)
 
 func cancel_moving():
 	$AnimatedSprite.stop()
